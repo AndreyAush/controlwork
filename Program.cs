7545389eb830e9a5f2c18ea22c49e9428ea540a9
Andrey <bakllorik90mail.ru>
2024-03-14 22:42:26 +0800

    добавлено решение задачи

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = new string[4] {"Hello", "123", "w", "12345"};
+string[] newarray = new string[array.Length];
+void SecondArrayWithIF(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newarray[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array, newarray);
+PrintArray(newarray);
